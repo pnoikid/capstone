852a28c77f75a7e5715213aefe7f5adbb2fa7bfa
pnoikid <rob.pena83@gmail.com>
2023-10-11 16:20:40 -0400

    added files

---
+++ src/components/SingleProduct.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { fetchSingleProduct } from "../api/api.js";
+
+export default function SingleProduct() {
+  const [singleProduct, setSingleProduct] = useState([]);
+  const [error, setError] = useState(null);
+  const params = useParams();
+  console.log("params: ", params.productId);
+  useEffect(() => {
+    async function singleProduct() {
+      try {
+        const data = await fetchSingleProduct(params.productId);
+        setSingleProduct(data);
+      } catch (error) {
+        setError(error);
+      }
+    }
+    fetchProduct();
+  }, [params.productId]);
+  console.log(singleProduct, "This is for a Single Product");
+  return (
+    <div>
+      <h1>Item</h1>
+      <img src={singleProduct.image} />
+      <div>{singleProduct.title}</div>
+      <div>{singleProduct.price}</div>
+      <div>{singleProduct.description}</div>
+      <div>{singleProduct.category}</div>
+      <Link to="/">Go Back </Link>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
